479fbdd15320f81133579f0244e047b449f1490f
pzych <pawel.zych@gmail.com>
2020-05-03 16:34:50 +0200

    2nd

---
+++ 2.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ * https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/564/
+ * 
+ * 
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+
+    return prices[0]
+};
+
+
+const prices = [7, 1, 5, 3, 6, 4];
+
+console.log(maxProfit(prices));
+
