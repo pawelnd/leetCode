149e7b17ef3e669da40be9386b7f63da92b97179
pzych <pawel.zych@gmail.com>
2020-05-05 21:41:55 +0200

    timeoput exceeded

--- 2.js
+++ 2.js
@@ -7,32 +7,27 @@
  * @return {number}
  */
 var maxProfit = function (prices) {
-    let currentProfit = 0;
-    calculateTransaction(prices, currentProfit);
+    return calculateTransaction(prices);
 
 };
 
 function calculateTransaction(prices) {
-    let profits = [];
+    let maxProfit = 0;
     for (let buy = 0; buy < prices.length; buy++) {
-        let max = 0;
         for (let sell = buy + 1; sell < prices.length; sell++) {
             const profitForTransaction = prices[sell] - prices[buy];
             const profitForRest = calculateTransaction(prices.slice(sell + 1));
-            console.log('translaction', buy, sell, 'profit');
-
-
+            if (maxProfit < profitForTransaction + profitForRest) {
+                maxProfit = profitForTransaction + profitForRest;
+            }
         }
-        // profits.sort(([a], [b]) => a - b);
-        // console.log(profits);
-        // profits = [];
-
     }
+    return maxProfit;
 }
 
 // const prices = [1, 2];
-// const prices = [7, 1, 5, 3, 6, 4];
-const prices = [7, 1, 5, 1, 12, 4];
+const prices = [7, 1, 5, 3, 6, 4];
+// const prices = [7, 1, 5, 1, 12, 4];
 
 console.log(maxProfit(prices));
 
