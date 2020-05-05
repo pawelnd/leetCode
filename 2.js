fe712e9ac047f7cae1756492f48ab09612b82b36
pzych <pawel.zych@gmail.com>
2020-05-05 20:56:38 +0200

    dasd

--- 2.js
+++ 2.js
@@ -7,12 +7,32 @@
  * @return {number}
  */
 var maxProfit = function (prices) {
+    let currentProfit = 0;
+    calculateTransaction(prices, currentProfit);
 
-    return prices[0]
 };
 
+function calculateTransaction(prices) {
+    let profits = [];
+    for (let buy = 0; buy < prices.length; buy++) {
+        let max = 0;
+        for (let sell = buy + 1; sell < prices.length; sell++) {
+            const profit = prices[sell] - prices[buy] + calculateTransaction(prices.slice(sell + 1));
 
-const prices = [7, 1, 5, 3, 6, 4];
+            console.log('translaction', buy, sell, 'profit');
+
+
+        }
+        // profits.sort(([a], [b]) => a - b);
+        // console.log(profits);
+        // profits = [];
+
+    }
+}
+
+// const prices = [1, 2];
+// const prices = [7, 1, 5, 3, 6, 4];
+const prices = [7, 1, 5, 1, 12, 4];
 
 console.log(maxProfit(prices));
 
