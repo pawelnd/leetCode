c0bb3511df466e7ce2bc2ae0d79ed9b133e180b0
pzych <pawel.zych@gmail.com>
2021-04-25 22:57:27 +0200

    dasdsasadada

--- math/fizbuz.ts
+++ math/fizbuz.ts
@@ -1,8 +1,9 @@
 function fizzBuzz(n: number): string[] {
     const result = []
-    for (let i = 1; i <= n; i++) {
-        result[i] = (i % 3 == 0 ? 'Fizz' : '') + (i % 5 == 0 ? 'Buzz' : '');
-        result[i] = result[i] =='' ? i : result[i];
+    for (let i = 0; i < n; i++) {
+        const num = i + 1;
+        result[i] = (num % 3 == 0 ? 'Fizz' : '') + (num % 5 == 0 ? 'Buzz' : '');
+        result[i] = result[i] == '' ? '' + num : result[i];
     }
     return result
 };
