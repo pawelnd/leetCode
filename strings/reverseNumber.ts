6be98cc75a6b9703369e430150f20a228e4bae7e
pzych <pawel.zych@gmail.com>
2021-04-26 08:05:59 +0200

    dasdsasadada

--- strings/reverseNumber.ts
+++ strings/reverseNumber.ts
@@ -1,28 +1,43 @@
-const MAX = Math.pow(2,31) - 1
-const MIN = Math.pow(-2,31)
+const MAX = Math.pow(2, 31) - 1
+const MIN = Math.pow(-2, 31)
+
 function reverseNumber(x: number): number {
     let rev = 0;
     while (x != 0) {
         let pop = x % 10;
-        x = (x / 10) >> 0;;
+        x = (x / 10) >> 0;
+        ;
         if (rev > (MAX / 10) >> 0 || (rev == (MAX / 10) >> 0 && pop > 7)) return 0;
-        if (rev < (MIN / 10 >>0) || (rev == (MIN / 10 >>0) && pop < -8)) return 0;
+        if (rev < (MIN / 10 >> 0) || (rev == (MIN / 10 >> 0) && pop < -8)) return 0;
         rev = rev * 10 + pop;
     }
     return rev;
 };
 
 
-console.log(reverseNumber(123))
-console.log(reverseNumber(-123))
-console.log(reverseNumber(120))
-console.log(reverseNumber(0))
-console.log(reverseNumber(-1563847412))
-console.log(reverseNumber(1534236469))
+function reverseNumber2(x: number): number {
+    const isNegative = x < 0;
+    let result = Math.abs(x).toString().split('').reverse().join('');
+    result = result.replace(/^0*/, '');
+    result = (isNegative ? '-' : '') + result;
+    result = result == '' ? '0' : result;
+    let resultNum = parseInt(result, 10);
 
-console.assert(reverseNumber(123) == -123)
-console.assert(reverseNumber(-123) == -321)
-console.assert(reverseNumber(120) == 21)
-console.assert(reverseNumber(0) == 0)
-console.assert(reverseNumber(1534236469) == 0)
-console.assert(reverseNumber(-1563847412) == 0)
+    return resultNum.toString() == result && resultNum >= MIN && resultNum <= MAX ? resultNum : 0;
+};
+
+
+// console.log(reverseNumber2(123))
+// console.log(reverseNumber2(-123))
+// console.log(reverseNumber2(120))
+// console.log(reverseNumber2(0))
+// console.log(reverseNumber2(-1563847412))
+console.log(reverseNumber2(1534236469))
+console.log(reverseNumber2(1111111111199999999999999))
+
+// console.assert(reverseNumber2(123) == -123)
+// console.assert(reverseNumber2(-123) == -321)
+// console.assert(reverseNumber2(120) == 21)
+// console.assert(reverseNumber2(0) == 0)
+// console.assert(reverseNumber2(-1563847412) == 0)
+console.assert(reverseNumber2(1534236469) == 0)
