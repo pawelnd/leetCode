c6c29def0bd3cf447bf6b5dd35f21029e298527a
pzych <pawel.zych@gmail.com>
2021-04-30 12:50:27 +0200

    dasdsasadada

--- strings/countAndSay.ts
+++ strings/countAndSay.ts
@@ -1,30 +1,36 @@
+function countSameCharsAs1stLetter(str: string): number {
+    if (str.length == 0){
+        throw new Error('Illegal arg. EMmpty string')
+    }
+
+    const first = str[0];
+    let counter = 0;
+    for (const letter of str) {
+        if (letter != first) {
+            break;
+        }
+        counter++;
+    }
+    return counter;
+}
+
 function countAndSay(n: number): string {
     let result = '1';
     for (let i = 1; i < n; i++) {
         let tmp = result;
         let nextResult = '';
         while (tmp.length > 0) {
-            console.log(tmp)
-            let index = ((t) => {
-                let counter = 0;
-                for (const character of t) {
-                    if (character === tmp[0]) {
-                        counter++;
-                    } else {
-                        break;
-                    }
-                }
-                return counter;
-            })(tmp)
-            nextResult += index + tmp[0];
-            tmp = tmp.substring(0, index-1 )
+            const position = countSameCharsAs1stLetter(tmp);
+            nextResult += position + tmp[0];
+            tmp = tmp.replace(tmp.substring(0, position),'')
         }
         result = nextResult;
+        console.log(' ----------------  ', result)
     }
     return result;
 };
 
-console.log(countAndSay(4))
+console.log(countAndSay(10))
 
 
 /*
